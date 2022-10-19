bbc0f51880848e637d45372c0778ae394b2a698f
lucas-lpss <lcss.lopes@hotmail.com>
2022-10-18 21:12:08 -0300

    conexaoComBanco

---
+++ src/main/java/Spring/two/demo1/Repository.java
@@ -0,0 +1,7 @@
+package Spring.two.demo1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<Cliente,Long> {
+
+}
---
+++ src/main/java/Spring/two/demo1/projectConexãoComBanco.java
@@ -0,0 +1,13 @@
+package Spring.two.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class projectConexãoComBanco {
+
+	public static void main(String[] args) {
+		SpringApplication.run(projectConexãoComBanco.class, args);
+	}
+
+}
